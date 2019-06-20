bbeeedc9156d3224943c1f11c749b28e8b15d34e
Filippo Rivolta <rivoltafilippo@gmail.com>
2019-06-20 15:02:52 +0200

    casehistory images

--- site/web/app/themes/filipporivolta/resources/views/partials/case-history/carousel-alt.blade.php
+++ site/web/app/themes/filipporivolta/resources/views/partials/case-history/carousel-alt.blade.php
@@ -3,9 +3,12 @@
       <img class="carousel-slider__item__image" src="@asset('images/budgety/budgety_01.jpg')" alt="budgety" />
     </div>
     <div class="carousel-slider__item">
-      <img class="carousel-slider__item__image" src="@asset('images/budgety/budgety_01.jpg')" alt="budgety" />
+      <img class="carousel-slider__item__image" src="@asset('images/budgety/budgety_02.jpg')" alt="budgety" />
     </div>
     <div class="carousel-slider__item">
-      <img class="carousel-slider__item__image" src="@asset('images/budgety/budgety_01.jpg')" alt="budgety" />
+      <img class="carousel-slider__item__image" src="@asset('images/budgety/budgety_03.jpg')" alt="budgety" />
+    </div>
+    <div class="carousel-slider__item">
+      <img class="carousel-slider__item__image" src="@asset('images/budgety/budgety_04.jpg')" alt="budgety" />
     </div>
   </section>
--- site/web/app/themes/filipporivolta/resources/views/partials/case-history/carousel.blade.php
+++ site/web/app/themes/filipporivolta/resources/views/partials/case-history/carousel.blade.php
@@ -4,7 +4,8 @@
         <div class="swiper-wrapper">
             <!-- Slides -->
             <div class="swiper-slide"><img src="@asset('images/budgety/budgety_01.jpg')" alt="budgety" /></div>
-            <div class="swiper-slide"><img src="@asset('images/budgety/budgety_01.jpg')" alt="budgety" /></div>
-            <div class="swiper-slide"><img src="@asset('images/budgety/budgety_01.jpg')" alt="budgety" /></div>
+            <div class="swiper-slide"><img src="@asset('images/budgety/budgety_02.jpg')" alt="budgety" /></div>
+            <div class="swiper-slide"><img src="@asset('images/budgety/budgety_03.jpg')" alt="budgety" /></div>
+            <div class="swiper-slide"><img src="@asset('images/budgety/budgety_04.jpg')" alt="budgety" /></div>
         </div>
 </div>
